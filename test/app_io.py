ad7ff188ac4acba0391ed49a93ae0ced677033e4
Pavlina Smolkova <smo0117@vsb.cz>
2022-06-19 23:03:53 +0200

    line colors with LineCollection

--- test/app_io.py
+++ test/app_io.py
@@ -1,22 +1,22 @@
 import pandas as pd
 
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 class Row:
-    def __init__(self, row):
-        self.timestamp = row[0][0]
-        self.from_node_id = int(row[0][1])
-        self.to_node_id = int(row[0][2])
-        self.vehicle_id = int(row[1][0])
-        self.start_offset_m = row[1][1]
-
     # def __init__(self, row):
-    #     self.timestamp = datetime.strptime(row[1][0], '%Y-%m-%d %H:%M:%S')
-    #     self.from_node_id = int(row[1][1])
-    #     self.to_node_id = int(row[1][2])
-    #     self.vehicle_id = int(row[1][3])
-    #     self.start_offset_m = row[1][4]
+    #     self.timestamp = row[0][0]
+    #     self.from_node_id = int(row[0][1])
+    #     self.to_node_id = int(row[0][2])
+    #     self.vehicle_id = int(row[1][0])
+    #     self.start_offset_m = row[1][1]
+
+    def __init__(self, row):
+        self.timestamp = datetime.strptime(row[1][0], '%Y-%m-%d %H:%M:%S')
+        self.from_node_id = int(row[1][1])
+        self.to_node_id = int(row[1][2])
+        self.vehicle_id = int(row[1][3])
+        self.start_offset_m = row[1][4]
 
     def __str__(self):
         # return str(f"_{self.timestamp}_, {self.from_node_id}, {self.to_node_id}, {self.vehicle_id},
@@ -34,10 +34,15 @@ class Segment:
         self.vehicle_count = int(vehicle_count)
         self.density = -1
         self.car_indexes = [int(car_index)]
-        self.car_offsets = []   # will be moved elsewhere later
+        self.car_offsets = []  # will be moved elsewhere later
 
     def __str__(self):
-        return str((self.from_node_id, self.to_node_id, self.timestamp, self.vehicle_count, self.car_indexes))
+        return str(
+            f"{str(self.timestamp)}, {self.from_node_id}, {self.to_node_id}, {self.vehicle_count},{self.car_indexes}")
+        # return str((self.from_node_id, self.to_node_id, str(self.timestamp), self.vehicle_count, self.car_indexes))
+
+    def __repr__(self):
+        return str(self)
 
 
 def load_row(rows, row_index, times):
@@ -85,7 +90,7 @@ def load_row_in_time(segments, row, current_time):
     return
 
 
-def load_input(path):
+def load_input(path, rows_from=0, rows_count=None):
     if path.endswith('pickle'):
         data = pd.read_pickle(path)
     else:
@@ -95,7 +100,9 @@ def load_input(path):
 
     times = []
 
-    for i in range(0, len(rows)):
+    max_range = rows_count + rows_from - 1 if rows_count is not None else len(rows)
+    max_range = min(len(rows), max_range)
+    for i in range(rows_from, max_range):
         print(i)
         load_row(rows, i, times)
 
--- test/app_plot.py
+++ test/app_plot.py
@@ -1,21 +1,27 @@
+import pprint
+
 import numpy
 import osmnx as ox
 import numpy as np
 
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
 
-def plot_segment_line(ax, x, y, width_from_m, width_to_m):
+
+def plot_segment_line_width(ax, x, y, width_from_m, width_to_m):
     if abs(x[0] - x[-1]) > abs(y[0] - y[-1]):
         d = np.linspace(width_from_m / 10000, width_to_m / 10000, len(y))
         y1 = np.add(y, d)
         y2 = np.subtract(y, d)
 
-        ax.fill_between(x, y1, y2, alpha=.5, linewidth=0, color='red', edgecolor='red')
+        ax.fill_between(x, y1, y2, alpha=.5, linewidth=0, color='red')
     else:
         d = numpy.linspace(width_from_m / 10000, width_to_m / 10000, len(x))
         x1 = np.add(x, d)
         x2 = np.subtract(x, d)
 
-        ax.fill_betweenx(y=y, x1=x1, x2=x2, alpha=.5, linewidth=0, color='red', edgecolor='red')
+        ax.fill_betweenx(y=y, x1=x1, x2=x2, alpha=.5, linewidth=0, color='red')
 
 
 def get_node_density(segments):
@@ -30,6 +36,19 @@ def get_node_density(segments):
     return node_densities
 
 
+def plot_segment_line_color(x, y, density_from, density_to, min_density=1, max_density=50, lw=2):
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    norm = plt.Normalize(min_density, max_density)
+    lc = LineCollection(segments, cmap='autumn_r', norm=norm)
+    # Set the values used for colormapping
+    density_array = np.linspace(density_from, density_to, len(x))
+    lc.set_array(density_array)
+    lc.set_linewidth(lw)
+    return lc
+
+
 def plot_segment_edge(
         G,
         u,
@@ -37,18 +56,22 @@ def plot_segment_edge(
         ax,
         density_from=0,
         density_to=0,
-        min_width=None,
+        line_width=None,
+        min_width_density=10,
+        max_width_density=50,
+        min_color_density=1,
+        max_color_density=10
 ):
     # get width for one car
-    if min_width is None:
-        min_width = min(ax._viewLim.width, ax._viewLim.height) * 2
+    if line_width is None:
+        line_width = min(ax._viewLim.width, ax._viewLim.height)
 
     x = []
     y = []
 
     edge = G.get_edge_data(u, v)
     if edge is None:
-        # print("Edge not found")
+        print("Edge not found")
         pass
     else:
         data = min(edge.values(), key=lambda d: d["length"])
@@ -60,15 +83,25 @@ def plot_segment_edge(
             x.extend((G.nodes[u]["x"], G.nodes[v]["x"]))
             y.extend((G.nodes[u]["y"], G.nodes[v]["y"]))
 
-        _ = ax.plot(x, y, lw=2, alpha=0.5, markersize=0, color='red')
+        # simple line
+        # _ = ax.plot(x, y, lw=2, alpha=0.5, markersize=0, color='red')
 
-        plot_segment_line(ax, x, y, density_from*min_width, density_to*min_width)
+        # colored line
+        lc = plot_segment_line_color(x, y, density_from, density_to, min_density=min_color_density, max_density=max_color_density)
+        ax.add_collection(lc)
+
+        # line with width
+        width_from = max(0, (density_from - min_width_density)) * 100 / max_width_density
+        width_to = max(0, (density_to - min_width_density)) * 100 / max_width_density
+        if max(width_from, width_to) > 0:
+            plot_segment_line_width(ax, x, y, width_from, width_to)
 
 
 def plot_graph_route(
         G,
         segments,
         ax=None,
+        max_count=10,
         **pg_kwargs,
 ):
     if ax is None:
@@ -95,5 +128,5 @@ def plot_graph_route(
         for u, v in zip(route[:-1], route[1:]):
             density_from = node_densities[u] if u in node_densities else 0
             density_to = node_densities[v] if v in node_densities else 0
-            plot_segment_edge(G, u, v, ax, density_from, density_to)
+            plot_segment_edge(G, u, v, ax, density_from, density_to, max_width_density=max_count)
     return
