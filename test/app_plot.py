7f89dd41ceaea1e38bbbfebd18d73c7c0d3f5d90
Pavlina Smolkova <smo0117@vsb.cz>
2022-08-18 10:10:46 +0200

    line with in form of a "caligraphy pen"

--- test/app_plot.py
+++ test/app_plot.py
@@ -1,42 +1,77 @@
-import pprint
-
 import numpy
 import osmnx as ox
 import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
-from matplotlib.colors import ListedColormap, BoundaryNorm
 
 
+# PLOTS LINE WITH AROUND LINES ABOVE MIN_WIDTH_DENSITY
+from app_plot_width import plot_line_width_equidistant
+
+
+def plot_route_width(ax, x, y,
+                     density_from, density_to,
+                     min_width_density, max_width_density,
+                     width_modifier=2,
+                     equidistant=False
+                     ):
+
+    width_from, width_to = np.interp([density_from, density_to],
+                                     [min_width_density, max_width_density], [0, 10 * width_modifier])
+
+    if max(width_from, width_to) > 0:
+        if not equidistant:
+            plot_segment_line_width(ax, x, y, width_from, width_to)
+        else:
+            plot_line_width_equidistant(ax, x, y, width_from, width_to)
+
+
+# COLORED SPACE EITHER HORIZONTALLY OR VERTICALLY AROUND LINES
 def plot_segment_line_width(ax, x, y, width_from_m, width_to_m):
     if abs(x[0] - x[-1]) > abs(y[0] - y[-1]):
         d = np.linspace(width_from_m / 10000, width_to_m / 10000, len(y))
         y1 = np.add(y, d)
         y2 = np.subtract(y, d)
 
-        ax.fill_between(x, y1, y2, alpha=.5, linewidth=0, color='red')
+        ax.fill_between(x, y1, y2, alpha=1, linewidth=0, color='red')
     else:
-        d = numpy.linspace(width_from_m / 10000, width_to_m / 10000, len(x))
+        d = np.linspace(width_from_m / 10000, width_to_m / 10000, len(x))
         x1 = np.add(x, d)
         x2 = np.subtract(x, d)
 
-        ax.fill_betweenx(y=y, x1=x1, x2=x2, alpha=.5, linewidth=0, color='red')
+        ax.fill_betweenx(y=y, x1=x1, x2=x2, alpha=1, linewidth=0, color='red')
 
 
-def get_node_density(segments):
-    node_densities = {}
-    for segment in segments:
-        if segment.from_node_id in node_densities:
-            node_densities[segment.from_node_id] += segment.vehicle_count
+# COUNT CAR DENSITY FOR NODE (WILL MOVE OUTSIDE OF PLOTTING FUNCTIONS)
+def get_density(segments, G=None):
+    densities = {}
+
+    def _add_density(node, count, node2='density'):
+        if node in densities:
+            densities[node][node2] += count
         else:
-            node_densities[segment.from_node_id] = segment.vehicle_count
+            densities[node] = {node2: count}
 
-    # print(node_densities)
-    return node_densities
+    if G is None:
+        for segment in segments:
+            _add_density(segment.from_node_id, segment.vehicle_count)
+    else:
+        for segment in segments:
+
+            if G.has_edge(segment.from_node_id, segment.to_node_id):
+                edge_length = G[segment.from_node_id][segment.to_node_id][0]['length']
+
+                for offset in segment.car_offsets:
+                    if offset < edge_length/2:
+                        _add_density(segment.from_node_id, 1)
+                    else:
+                        _add_density(segment.to_node_id, 1)
+    return densities
 
 
-def plot_segment_line_color(x, y, density_from, density_to, min_density=1, max_density=50, lw=2):
+# PLOTS COLOR GRADIENT ON ONE EDGE
+def plot_segment_line_color(x, y, density_from, density_to, min_density, max_density, lw=2):
     points = np.array([x, y]).T.reshape(-1, 1, 2)
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
@@ -49,31 +84,16 @@ def plot_segment_line_color(x, y, density_from, density_to, min_density=1, max_d
     return lc
 
 
-def plot_segment_edge(
-        G,
-        u,
-        v,
-        ax,
-        density_from=0,
-        density_to=0,
-        line_width=None,
-        min_width_density=10,
-        max_width_density=50,
-        min_color_density=1,
-        max_color_density=10
+# PLOTS ONE EDGE OF GRAPH
+def plot_segment_edge(G, u, v, ax, density_from, density_to,
+                      min_color_density=1, max_color_density=10,
+                      min_width_density=10, max_width_density=50
 ):
-    # get width for one car
-    if line_width is None:
-        line_width = min(ax._viewLim.width, ax._viewLim.height)
-
     x = []
     y = []
 
     edge = G.get_edge_data(u, v)
-    if edge is None:
-        print("Edge not found")
-        pass
-    else:
+    if edge is not None:
         data = min(edge.values(), key=lambda d: d["length"])
         if "geometry" in data:
             xs, ys = data["geometry"].xy
@@ -88,22 +108,14 @@ def plot_segment_edge(
 
         # colored line
         lc = plot_segment_line_color(x, y, density_from, density_to, min_density=min_color_density, max_density=max_color_density)
-        ax.add_collection(lc)
+        ax.add_collection(lc, autolim=False)
 
-        # line with width
-        width_from = max(0, (density_from - min_width_density)) * 100 / max_width_density
-        width_to = max(0, (density_to - min_width_density)) * 100 / max_width_density
-        if max(width_from, width_to) > 0:
-            plot_segment_line_width(ax, x, y, width_from, width_to)
+        # line with width if density above min_width_density
+        plot_route_width(ax, x, y, density_from, density_to, min_width_density, max_width_density)
 
 
-def plot_graph_route(
-        G,
-        segments,
-        ax=None,
-        max_count=10,
-        **pg_kwargs,
-):
+# MAIN PLOT FUNCTION
+def plot_segments(G, segments, ax=None, **pg_kwargs):
     if ax is None:
         override = {"show", "save", "close"}
         kwargs = {k: v for k, v in pg_kwargs.items() if k not in override}
@@ -111,8 +123,8 @@ def plot_graph_route(
     else:
         fig = ax.figure
 
-    # Add node densities
-    node_densities = get_node_density(segments)
+    # Count node densities
+    node_densities = get_density(segments, G)
 
     for segment in segments:
         if segment.from_node_id not in G:
@@ -126,7 +138,6 @@ def plot_graph_route(
         # route = [segment.from_node_id, segment.to_node_id]
         route = ox.shortest_path(G, segment.from_node_id, segment.to_node_id)
         for u, v in zip(route[:-1], route[1:]):
-            density_from = node_densities[u] if u in node_densities else 0
-            density_to = node_densities[v] if v in node_densities else 0
-            plot_segment_edge(G, u, v, ax, density_from, density_to, max_width_density=max_count)
-    return
+            density_from = node_densities[u]['density'] if u in node_densities else 0
+            density_to = node_densities[v]['density'] if v in node_densities else 0
+            plot_segment_edge(G, u, v, ax, density_from, density_to)
