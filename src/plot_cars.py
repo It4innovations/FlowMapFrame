2f7a5866e09339d72cd0d29cf45061e558849dc2
Pavlina Smolkova <smo0117@vsb.cz>
2022-08-30 15:32:19 +0200

    adjust plot_cars to dataframe input

--- src/plot_cars.py
+++ src/plot_cars.py
@@ -1,21 +1,22 @@
 import numpy as np
+import pandas as pd
 
 from collection_plot import get_node_coordinates
 
 
 def get_cars_xy(G, s):
 
-    edge = G.get_edge_data(s.from_node_id, s.to_node_id)
+    edge = G.get_edge_data(s['node_from'], s['node_to'])
 
     if edge is None:
         return [], []
     else:
         x, y = get_node_coordinates(edge, G, s)
 
-        edge_length = G[s.from_node_id][s.to_node_id][0]['length']
+        edge_length = G[s['node_from']][s['node_to']][0]['length']
         scale = np.linspace(0, edge_length, len(x))
-        cars_x = np.interp(s.car_offsets, scale, x)
-        cars_y = np.interp(s.car_offsets, scale, y)
+        cars_x = np.interp(s['start_offset_m'], scale, x)
+        cars_y = np.interp(s['start_offset_m'], scale, y)
 
         return cars_x, cars_y
 
@@ -24,10 +25,13 @@ def plot_cars(G, segments, ax):
     xp = []
     yp = []
 
-    for s in segments:
-        x, y = get_cars_xy(G, s)
-        xp.extend(x)
-        yp.extend(y)
+    if isinstance(segments, pd.Series):
+        x, y = get_cars_xy(G, segments)
+    else:
+        for _, s in segments.iterrows():
+            x, y = get_cars_xy(G, s)
+            xp.extend(x)
+            yp.extend(y)
 
     ax.scatter(xp, yp)
 
