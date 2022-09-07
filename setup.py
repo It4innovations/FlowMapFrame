bf3bb0f6b3b31ec1567e4bbcc47376a84ddd9531
Pavlina Smolkova <smo0117@vsb.cz>
2022-09-07 15:07:51 +0200

    added setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flowmapviz',
+    version='0.1',
+    packages=find_packages(),
+    author='Pavlina Smolkova',
+    author_email='smo0117@vsb.cz',
+    description='Package for plotting routes',
+    # install_requires=[osmnx, mathplotlib, pandas, click, numpy, shapely]
+)
