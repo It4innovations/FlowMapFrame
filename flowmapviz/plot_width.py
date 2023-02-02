e83a98c5e595e70a8fbef82ec278ab891d3984be
Pavlina Smolkova <smo0117@vsb.cz>
2023-02-02 17:44:40 +0100

    prolong the equidistant into full length

--- flowmapviz/plot_width.py
+++ flowmapviz/plot_width.py
@@ -6,10 +6,9 @@ def plot_line_width_equidistant(ax, x, y, width_from_m, width_to_m):
 
     width_from_m = width_from_m / 10000
     width_to_m = width_to_m / 10000
-    line = LineString(zip(x, y))
 
     distances = np.linspace(width_from_m, width_to_m, len(x))
-    x_eq, y_eq, x_eq2, y_eq2 = eq(x, y, distances)
+    x_eq, y_eq, x_eq2, y_eq2 = calculate_equidistant_coords(x, y, distances)
 
     plot_polygon_between_lines(ax, x_eq, y_eq, x_eq2, y_eq2)
 
@@ -32,8 +31,7 @@ def plot_polygon_between_lines(ax, x1, y1, x2, y2):
     plot_polygons(ax, pol)
 
 
-def eq(x, y, distances):
-    line = LineString(zip(x, y))
+def calculate_equidistant_coords(x, y, distances):
     # Mid points
     x_m = []
     y_m = []
@@ -45,10 +43,23 @@ def eq(x, y, distances):
         y_m.append((y[i + 1] - y[i]) / 2.0 + y[i])
         x_m.append((x[i + 1] - x[i]) / 2.0 + x[i])
         d_m.append((distances[i + 1] - distances[i]) / 2.0 + distances[i])
+
         if y[i + 1] == y[i]:
             k_m.append(1e8)
         else:
             k_m.append(-(x[i + 1] - x[i]) / (y[i + 1] - y[i]))
+
+    # Add first elements
+    x_m.insert(0, x[0])
+    y_m.insert(0, y[0])
+    d_m.insert(0, distances[0])
+    k_m.insert(0, k_m[0])
+
+    # Add last elements
+    x_m.append(x[-1])
+    y_m.append(y[-1])
+    d_m.append(distances[-1])
+    k_m.append(k_m[-1])
 
     # Convert into np.arrays
     x_m = np.array(x_m)
@@ -69,9 +80,13 @@ def eq(x, y, distances):
         y_eq[i] = (y_m[i] - k_m[i] * x_m[i]) + k_m[i] * x_eq[i]
         y_eq2[i] = (y_m[i] - k_m[i] * x_m[i]) + k_m[i] * x_eq2[i]
 
-    for i in range(1, len(x_eq) - 1):
+    # Switch lines that intersect the original line
+    original_line = LineString(zip(x, y))
+
+    for i in range(1, len(x_eq)):
         seg = LineString(zip(x_eq[i-1:i+1], y_eq[i-1:i+1]))
-        if line.intersection(seg):
+
+        if original_line.intersection(seg):
             x_eq[i], x_eq2[i] = x_eq2[i], x_eq[i]
             y_eq[i], y_eq2[i] = y_eq2[i], y_eq[i]
 
